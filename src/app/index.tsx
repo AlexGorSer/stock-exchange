af7322ebc43839e30dba26c93d5c3a53fc358432
AlexGorSer <taskekaminari@gmail.com>
2023-07-28 19:58:37 +0200

    feat: add base prettier

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,33 +1,30 @@
-import { Box, Text, Flex, Button, useColorMode } from "@chakra-ui/react";
-import { FC } from "react";
+import { Box, Text, Flex, Button, useColorMode } from '@chakra-ui/react'
+import { FC } from 'react'
 
 export const App: FC = () => {
-  const { toggleColorMode } = useColorMode();
+  const { toggleColorMode } = useColorMode()
   return (
     <>
       <Flex
         as="main"
-        display={"flex"}
+        display={'flex'}
         justifyContent="center"
         alignItems="center"
       >
         <Box
-          flexDir={"column"}
-          boxSize={"2xl"}
-          display={"flex"}
+          flexDir={'column'}
+          boxSize={'2xl'}
+          display={'flex'}
           justifyContent="center"
           alignItems="center"
-          gap={"1rem"}
+          gap={'1rem'}
         >
-          <Text
-            as={"h1"}
-            textAlign="center"
-          >
+          <Text as={'h1'} textAlign="center">
             Начальная страница
           </Text>
           <Button onClick={toggleColorMode}>Color Mode:</Button>
         </Box>
       </Flex>
     </>
-  );
-};
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client'
+import { App } from './app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
+createRoot(document.getElementById('root')!).render(
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>
+)
