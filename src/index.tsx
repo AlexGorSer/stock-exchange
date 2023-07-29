6a1804f218701eae49df1a75118f0fdc80f892f3
AlexGorSer <taskekaminari@gmail.com>
2023-07-29 20:01:28 +0200

    feat: add chakra config

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
-
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
-
-const theme = extendTheme({ colors })
+import { theme } from './shared/config/ChakraConfig'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
---
+++ src/shared/index.ts
@@ -0,0 +1,3 @@
+import { theme } from './config/ChakraConfig'
+
+export { theme }
